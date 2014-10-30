31e2e4ab1823a7799001680421d14fde32d3ed97
Alex Bepple <alex@bepple.de>
2014-10-30 19:27:26 +0100

    fail better

--- test/unit/end_detector_spec.js
+++ test/unit/end_detector_spec.js
@@ -1,23 +1,23 @@
 var _ = require('lodash');
-var endDetector = require('end_detector');
+var sinon = require('sinon');
+var end = require('end_detector');
 
 describe('End detector', function() {
 	it('returns date from date detector using input minus 1st token', function() {
-		// rewrite test so that error messages are understandable!!!
 		var dateFromDateDetector = Date.create();
-		var stubDetectDate = function(input) {
-			if (input === 'bar') return {date: dateFromDateDetector};
-			return {date: Date.create('invalid'), tail: 'invalid result'};
-		};
-		var detectEnd = endDetector.detector(stubDetectDate);
+		var dateDetectorStub = sinon.stub();
+		dateDetectorStub.withArgs('bar').returns({date: dateFromDateDetector});
+		dateDetectorStub.returns({date: "'wrong input for date detector'"});
+		var detectEnd = end.detector(dateDetectorStub);
 
 		detectEnd('foo bar').date.should.equal(dateFromDateDetector);
 	});
 	it('uses tail of date detector when it finds a date', function() {
-		var resultWithValidDate = {date: Date.create(), tail: 'tail from date detector'};
-		detectEnd = endDetector.detector(function() {
-			return resultWithValidDate;
-		});
+		var resultWithValidDate = {
+			date: Date.create(), 
+			tail: 'tail from date detector'
+		};
+		detectEnd = end.detector(function() {return resultWithValidDate;});
 		detectEnd('input').tail.should.equal('tail from date detector');
 	});
 	it('returns the input as the tail when it cannot find a date', function() {
@@ -25,7 +25,7 @@ describe('End detector', function() {
 			date: Date.create('invalid'),
 			tail: 'tail from date detector'
 		};
-		var detectEnd = endDetector.detector(function(input) {
+		var detectEnd = end.detector(function(input) {
 			return dateDetectorResultWithInvalidDate;
 		});
 
