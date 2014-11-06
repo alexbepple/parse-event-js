78a6bae211d4522423e265bf24ec223feb234cea
Alex Bepple <alex@bepple.de>
2014-11-06 23:15:54 +0100

    make code easier to understand

--- src/date_parser2.js
+++ src/date_parser2.js
@@ -5,20 +5,16 @@ var future = function(dateSpec, reference) {
     var result;
 
     result = moment(dateSpec, 'H:mm');
+    if (r.isEmpty(result.parsingFlags().unusedTokens)) return result;
 
-    if (!r.isEmpty(result.parsingFlags().unusedTokens)) {
-        result = moment(dateSpec, 'D MMM');
-        if (result.isValid() && !r.isEmpty(result.parsingFlags().unusedInput)) return moment.invalid();
-    }
-    if (!result.isValid()) {
-        result = reference || moment();
-        result.day(dateSpec);
-        result.startOf('day');
-    }
-    if (!r.isEmpty(result.parsingFlags().unusedTokens)) {
-        result = moment(dateSpec, 'H:mm');
-    }
-    return result;
+    result = moment(dateSpec, 'D MMM');
+    if (result.isValid() && !r.isEmpty(result.parsingFlags().unusedInput)) return moment.invalid();
+    if (result.isValid()) return result;
+
+    result = reference || moment();
+    result.day(dateSpec);
+    result.startOf('day');
+    if (r.isEmpty(result.parsingFlags().unusedTokens)) return result;
 };
 
 module.exports = {
