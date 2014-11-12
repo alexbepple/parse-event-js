88ed574201eb17b849cd76e7cc950a3782b4565b
Alex Bepple <alex@bepple.de>
2014-11-12 19:43:29 +0100

    catch problems masked by NODE_PATH in tests

--- test/smoke/parseEvent_spec.js
+++ test/smoke/parseEvent_spec.js
@@ -1,10 +1,14 @@
 var exec = require('child_process').exec;
+var r = require('ramda');
 
 describe('parseEvent binary', function () {
 	it('exits with error code 0', function (done) {
-		var parseEvent = '/usr/bin/env node $HOME/engagements/calfred/parseEvent/src/parseEvent.js ""';
-		exec(parseEvent, function (error) {
-			expect(error).to.beNull(error);
+		var parseEvent = '/usr/bin/env node src/parseEvent.js ""';
+
+		var minimalEnv = r.pick(['PATH', 'PWD'])(process.env);
+
+		exec(parseEvent, {env: minimalEnv}, function (error) {
+			expect(error).to.beNull();
 			done();
 		});
 	});
