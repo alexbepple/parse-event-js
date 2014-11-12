b41357f18851a7b86d777a295a66c053aab3908b
Alex Bepple <alex@bepple.de>
2014-11-12 19:07:12 +0100

    ask environment for Node executable

--- test/smoke/parseEvent_spec.js
+++ test/smoke/parseEvent_spec.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 describe('parseEvent binary', function () {
 	it('exits with error code 0', function (done) {
-		var parseEvent = '/usr/local/bin/node $HOME/engagements/calfred/parseEvent/src/parseEvent.js ""';
+		var parseEvent = '/usr/bin/env node $HOME/engagements/calfred/parseEvent/src/parseEvent.js ""';
 		exec(parseEvent, function (error) {
 			expect(error).to.beNull(error);
 			done();
