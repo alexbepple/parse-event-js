8d15b9a5208001d190b4b894b2cfd007c588b209
Alex Bepple <alex@bepple.de>
2014-11-13 23:22:02 +0100

    simplify code

--- src/end_detector.js
+++ src/end_detector.js
@@ -2,16 +2,14 @@ var moment = require('moment');
 var m = require('./misc');
 var r = require('ramda');
 
-var endDetector = function(detectDate) {
-	return function(input) {
-		var skipFirstToken = r.pipe(m.split, r.skip(1), m.join);
-		var detectEnd = r.pipe(skipFirstToken, detectDate);
-		var endMatch = detectEnd(input);
-		if (!moment(endMatch.date).isValid()) endMatch.tail = input;
-		return endMatch;
-	};
+var endDetector = function(detectDate, input) {
+    var skipFirstToken = r.pipe(m.split, r.skip(1), m.join);
+    var detectEnd = r.pipe(skipFirstToken, detectDate);
+    var endMatch = detectEnd(input);
+    if (!moment(endMatch.date).isValid()) endMatch.tail = input;
+    return endMatch;
 };
 
 module.exports = {
-	detector: endDetector
+	detector: r.curry(endDetector)
 };
